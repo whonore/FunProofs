eaf51a7eaa8586cc09267c763d21006bc434e179
whonore <wolfhonore@gmail.com>
2020-11-10 11:37:52 -0500

    Move set theory to library

---
+++ Lib/Sets.v
@@ -0,0 +1,254 @@
+From Coq Require Import
+  Lia
+  List
+  ListSet
+  Morphisms
+  RelationClasses.
+From FunProofs.Lib Require Import
+  EqDec
+  Logic
+  Tactics.
+Import EqDecNotations.
+
+Section Sets.
+  Context {A : Type}.
+  Context `{EqDec A}.
+
+  Definition set := {s : list A & NoDup s}.
+
+  Definition empty : set := existT _ nil (NoDup_nil _).
+
+  Definition add (x : A) (s : set) : set :=
+    existT _
+      (set_add eq_dec x (projT1 s))
+      (set_add_nodup _ x (projT2 s)).
+
+  Definition remove (x : A) (s : set) : set :=
+    existT _
+      (set_remove eq_dec x (projT1 s))
+      (set_remove_nodup _ x (projT2 s)).
+
+  Definition union (s s' : set) : set :=
+    existT _
+      (set_union eq_dec (projT1 s) (projT1 s'))
+      (set_union_nodup _ (projT2 s) (projT2 s')).
+
+  Definition intersect (s s' : set) : set :=
+    existT _
+      (set_inter eq_dec (projT1 s) (projT1 s'))
+      (set_inter_nodup _ (projT2 s) (projT2 s')).
+
+  Definition subtract (s s' : set) : set :=
+    existT _
+      (set_diff eq_dec (projT1 s) (projT1 s'))
+      (set_diff_nodup _ (projT2 s) (projT2 s')).
+
+  Definition card (s : set) : nat := length (projT1 s).
+
+  Definition In (x : A) (s : set) : Prop := In x (projT1 s).
+  Definition member (x : A) (s : set) : bool := set_mem eq_dec x (projT1 s).
+
+  Definition subset (s s' : set) : Prop := incl (projT1 s) (projT1 s').
+
+  Global Instance subset_refl : Reflexive subset.
+  Proof. red; intros; apply incl_refl. Qed.
+
+  Global Instance subset_trans : Transitive subset.
+  Proof. red; intros; eapply incl_tran; eauto. Qed.
+
+  Instance subset_preorder : PreOrder subset.
+  Proof. constructor; typeclasses eauto. Qed.
+
+  Definition equiv (s s' : set) : Prop := subset s s' /\ subset s' s.
+
+  Global Instance equiv_refl : Reflexive equiv.
+  Proof. split; reflexivity. Qed.
+
+  Global Instance equiv_sym : Symmetric equiv.
+  Proof. unfold equiv; split; intuition auto. Qed.
+
+  Global Instance equiv_trans : Transitive equiv.
+  Proof. unfold equiv; split; etransitivity; intuition eauto. Qed.
+
+  Global Instance equiv_preorder : PreOrder equiv.
+  Proof. constructor; typeclasses eauto. Qed.
+
+  Global Instance equiv_equivalence : Equivalence equiv.
+  Proof. constructor; typeclasses eauto. Qed.
+
+  Fixpoint _disjoint (s1 s2 : list A) : bool :=
+    match s1 with
+    | nil => true
+    | x :: s1' =>
+      if in_dec eq_dec x s2 then false else _disjoint s1' s2
+    end.
+  Definition disjoint (s1 s2 : set) : bool := _disjoint (projT1 s1) (projT1 s2).
+
+  Ltac setind s :=
+    let Hs := fresh "H" s in
+    intros until s; destruct s as (s & Hs);
+    induction s as [| el s]; cbn [projT1 projT2]; [|
+      match type of Hs with
+      | NoDup (?x :: ?xs) =>
+        let H := fresh Hs in
+        let Hnin := fresh "Hnin" in
+        assert (~List.In x xs /\ NoDup xs) as (Hnin & H) by (now inv Hs)
+      end].
+
+  Lemma member_in_true : forall x s,
+    member x s = true <-> In x s.
+  Proof. split; [apply set_mem_correct1 | apply set_mem_correct2]. Qed.
+
+  Corollary member_in_false : forall x s,
+    member x s = false <-> ~In x s.
+  Proof. intros; apply iff_not_true, member_in_true. Qed.
+
+  Lemma remove_in_card : forall x s,
+    In x s ->
+    card s = S (card (remove x s)).
+  Proof.
+    setind s; cbn in *; intros * Hin; intuition; simplify; auto.
+    rewrite eq_dec_false by intuition (subst; auto); auto.
+  Qed.
+
+  Lemma remove_not_in_card : forall x s,
+    ~In x s ->
+    card s = card (remove x s).
+  Proof.
+    setind s; cbn in *; intros * Hin; intuition.
+    destruct (_ == _); subst; cbn; intuition.
+  Qed.
+
+  Corollary card_unroll : forall s x,
+    card s = (if member x s then 1 else 0) + card (remove x s).
+  Proof.
+    intros; destruct (member _ _) eqn:Hin.
+    - now apply remove_in_card, member_in_true.
+    - now apply remove_not_in_card, member_in_false.
+  Qed.
+
+  Lemma subset_card : forall s s',
+    subset s s' -> card s <= card s'.
+  Proof. intros; destruct s; apply NoDup_incl_length; auto. Qed.
+
+  Lemma equiv_card : forall s s',
+    equiv s s' -> card s = card s'.
+  Proof.
+    intros * (Hsub1 & Hsub2).
+    apply subset_card in Hsub1; apply subset_card in Hsub2; lia.
+  Qed.
+
+  Lemma subset_empty_l : forall s, subset empty s.
+  Proof. intros; apply incl_nil_l. Qed.
+
+  Lemma subset_empty : forall s,
+    subset s empty -> equiv s empty.
+  Proof. intros * Hsub; split; auto using subset_empty_l. Qed.
+
+  Lemma equiv_empty_nil : forall s,
+    equiv s empty -> projT1 s = nil.
+  Proof.
+    destruct s as ([| x ss] & ?); auto; cbn; intros (Hsub & _).
+    now specialize (Hsub _ ltac:(cbn; auto)).
+  Qed.
+
+  Lemma subset_alt : forall s s',
+    subset s s' <-> forall x, In x s -> In x s'.
+  Proof. intuition auto. Qed.
+
+  Lemma remove_in : forall s x y,
+    In x (remove y s) ->
+    In x s.
+  Proof. destruct s; cbn; intros; eapply set_remove_1; eauto. Qed.
+
+  Lemma remove_not_in : forall s x y,
+    In x (remove y s) ->
+    x <> y.
+  Proof. destruct s; cbn; intros; eapply set_remove_2; eauto. Qed.
+
+  Lemma remove_subset : forall s x, subset (remove x s) s.
+  Proof.
+    destruct s; intros; rewrite subset_alt; intros * Hin.
+    eapply set_remove_1; cbn in *; eauto.
+  Qed.
+
+  Lemma subset_remove_remove : forall s ss x,
+    subset ss s ->
+    subset (remove x ss) (remove x s).
+  Proof.
+    intros *; rewrite !subset_alt; intros Hsub * Hin.
+    apply remove_not_in in Hin as Hneq.
+    apply set_remove_1 in Hin.
+    apply Hsub in Hin.
+    apply set_remove_3; auto.
+  Qed.
+
+  Lemma subset_remove : forall s ss x,
+    subset ss s ->
+    subset (remove x ss) s.
+  Proof. intros * Hsub; transitivity ss; auto using remove_subset. Qed.
+
+  Lemma _disjoint_false_correct : forall s1 s2,
+    _disjoint s1 s2 = false <->
+    (exists x, List.In x s1 /\ List.In x s2).
+  Proof.
+    induction s1 as [| x s1]; cbn; split; intros Hdisj; try easy.
+    - now destruct Hdisj.
+    - destruct (in_dec _ _ _); eauto.
+      rewrite IHs1 in Hdisj; destruct Hdisj; intuition eauto.
+    - destruct (in_dec _ _ _); eauto.
+      rewrite IHs1; destruct Hdisj; now intuition (subst; eauto).
+  Qed.
+
+  Corollary _disjoint_true_correct : forall s1 s2,
+    _disjoint s1 s2 = true <->
+    (forall x, ~(List.In x s1 /\ List.In x s2)).
+  Proof.
+    intros; rewrite <- not_exist; apply iff_not_false, _disjoint_false_correct.
+  Qed.
+
+  Corollary disjoint_false_correct : forall s1 s2,
+    disjoint s1 s2 = false <->
+    (exists x, In x s1 /\ In x s2).
+  Proof. intros; apply _disjoint_false_correct. Qed.
+
+  Corollary disjoint_true_correct : forall s1 s2,
+    disjoint s1 s2 = true <->
+    (forall x, ~(In x s1 /\ In x s2)).
+  Proof. intros; apply _disjoint_true_correct. Qed.
+
+  Lemma set_ind : forall (P : set -> Prop),
+    (forall s, equiv s empty -> P s) ->
+    (forall x s s', In x s -> equiv s' (remove x s) -> P s' -> P s) ->
+    forall s, P s.
+  Proof.
+    intros * Hempty IH; setind s.
+    - apply Hempty, subset_empty; hnf; auto.
+    - eapply IH; cbn; eauto.
+      instantiate (1 := ltac:(auto)).
+      split; hnf; cbn; intros *; rewrite eq_dec_true by auto; auto.
+  Qed.
+
+  Global Instance subset_equiv_morph : Proper (equiv ==> equiv ==> iff) subset.
+  Proof.
+    unfold equiv; repeat (hnf; intros).
+    intuition auto; repeat (etransitivity; eauto).
+  Qed.
+
+  Global Instance card_equiv_morph : Proper (equiv ==> eq) card.
+  Proof. now repeat (hnf; intros); apply equiv_card. Qed.
+End Sets.
+
+Arguments set _ : clear implicits.
+Global Opaque
+  set
+  empty
+  add
+  remove
+  union
+  intersect
+  subtract
+  card
+  In
+  member
+  subset.
--- Quorum.v
+++ Quorum.v
@@ -3,9 +3,8 @@
  * Two quorums have a non-empty intersection.
  *)
 
-From Coq Require Import
-  ListSet.
 From FunProofs.Lib Require Import
+  Sets
   Util.
 Import EqDecNotations.
 
@@ -13,112 +12,50 @@ Section Intersect.
   Variable A : Type.
   Context `{EqDec A}.
 
-  Lemma set_remove_length_in : forall x s,
-    NoDup s -> In x s ->
-    length s = S (length (set_remove eq_dec x s)).
+  Lemma disjoint_subset_card : forall (s ss1 ss2 : set A),
+    subset ss1 s -> subset ss2 s ->
+    ~(exists x, Sets.In x ss1 /\ Sets.In x ss2) ->
+    card ss1 + card ss2 <= card s.
   Proof.
-    induction s as [| y s]; cbn; intros * Hset Hin; intuition; simplify; auto.
-    inv Hset; rewrite eq_dec_false, IHs; cbn; intuition (subst; auto).
-  Qed.
-
-  Lemma set_remove_length_not_in : forall x s,
-    NoDup s -> ~In x s ->
-    length s = length (set_remove eq_dec x s).
-  Proof.
-    induction s as [| y s]; cbn; intros * Hset Hin; intuition.
-    inv Hset; destruct (x == y); subst; cbn; intuition.
-  Qed.
-
-  Lemma incl_nil : forall (s : list A),
-    incl s nil -> s = nil.
-  Proof.
-    unfold incl; intros * Hsub; destruct s as [| x ss]; auto.
-    now specialize (Hsub _ ltac:(cbn; auto)).
-  Qed.
-
-  Lemma incl_remove : forall s ss (x : A),
-    NoDup s -> NoDup ss -> incl ss (x :: s) ->
-    incl (set_remove eq_dec x ss) s.
-  Proof.
-    unfold incl; intros * Hset Hset1 Hsub * Hin.
-    apply set_remove_2 in Hin as Hneq; auto.
-    apply set_remove_1, Hsub in Hin; cbn in Hin.
-    now intuition (subst; auto).
-  Qed.
-
-  Fixpoint disjoint (s1 s2 : list A) :=
-    match s1 with
-    | nil => true
-    | x :: s1' =>
-      if in_dec eq_dec x s2 then false else disjoint s1' s2
-    end.
-
-  Lemma disjoint_correct : forall s1 s2,
-    disjoint s1 s2 = false <->
-    (exists x, In x s1 /\ In x s2).
-  Proof.
-    induction s1 as [| x s1]; cbn; split; intros Hdisj; try easy.
-    - now destruct Hdisj.
-    - destruct (in_dec _ _ _); eauto.
-      rewrite IHs1 in Hdisj; destruct Hdisj; intuition eauto.
-    - destruct (in_dec _ _ _); eauto.
-      rewrite IHs1; destruct Hdisj; now intuition (subst; eauto).
-  Qed.
-
-  Lemma length_unroll : forall s x,
-    NoDup s ->
-    length s = (if in_dec eq_dec x s then 1 else 0) + length (set_remove eq_dec x s).
-  Proof.
-    induction s as [| x s]; intros * Hset; inv Hset; auto.
-    destruct (in_dec _ _ _); cbn in *; intuition (simplify; auto).
-    - rewrite eq_dec_false; cbn; [| now intros ->].
-      rewrite <- set_remove_length_in; auto.
-    - cbn; rewrite <- set_remove_length_not_in; auto.
-  Qed.
-
-  Lemma disjoint_subset_length : forall (s ss1 ss2 : list A),
-    NoDup s -> NoDup ss1 -> NoDup ss2 ->
-    incl ss1 s -> incl ss2 s ->
-    ~(exists x, In x ss1 /\ In x ss2) ->
-    length ss1 + length ss2 <= length s.
-  Proof.
-    induction s as [| x s];
-      intros * Hset Hset1 Hset2 Hsub1 Hsub2 Hdisj; cbn; auto; inv Hset.
-    { apply incl_nil in Hsub1; apply incl_nil in Hsub2; subst; auto. }
-    rewrite (length_unroll ss1 x), (length_unroll ss2 x); auto.
-    repeat destruct (in_dec _ _ _); cbn.
+    induction s using set_ind; intros * Hsub1 Hsub2 Hdisj.
+    { prename equiv into Heq; rewrite Heq in Hsub1, Hsub2.
+      apply subset_empty in Hsub1; apply subset_empty in Hsub2.
+      rewrite Hsub1, Hsub2, Heq; auto.
+    }
+    prename A into x; prename equiv into Heq; prename (Sets.In _ _) into Hin.
+    apply member_in_true in Hin.
+    rewrite (card_unroll ss1 x), (card_unroll ss2 x), (card_unroll s1 x), <- Heq, Hin.
+    apply member_in_true in Hin.
+    destruct (member _ ss1) eqn:Hin1, (member _ ss2) eqn:Hin2;
+      rewrite ?member_in_true, ?member_in_false in Hin1;
+      rewrite ?member_in_true, ?member_in_false in Hin2; cbn.
     - exfalso; eauto.
-    - apply le_n_S, IHs; auto using set_remove_nodup, incl_remove.
-      intros (? & ? & ?).
-      eapply Hdisj; eauto using set_remove_1.
-    - rewrite Nat.add_comm; apply le_n_S, IHs; auto using set_remove_nodup, incl_remove.
-      intros (? & ? & ?).
-      eapply Hdisj; eauto using set_remove_1.
-    - etransitivity; [eapply IHs |]; auto using set_remove_nodup, incl_remove.
-      intros (? & ? & ?).
-      eapply Hdisj; eauto using set_remove_1.
+    - apply le_n_S, IHs1; rewrite ?Heq; auto using subset_remove_remove.
+      intros ?; destr *; eapply Hdisj; eauto using remove_in.
+    - rewrite Nat.add_comm; apply le_n_S, IHs1; rewrite ?Heq; auto using subset_remove_remove.
+      intros ?; destr *; eapply Hdisj; eauto using remove_in.
+    - etransitivity; [eapply IHs1 |]; rewrite ?Heq; auto using subset_remove_remove.
+      intros ?; destr *; eapply Hdisj; eauto using remove_in.
   Qed.
 
-  Lemma quorum_intersect : forall (s ss1 ss2 : list A),
-    NoDup s -> NoDup ss1 -> NoDup ss2 ->
-    incl ss1 s -> incl ss2 s ->
-    length s < length ss1 + length ss2 ->
-    exists x, In x ss1 /\ In x ss2.
+  Lemma quorum_intersect : forall (s ss1 ss2 : set A),
+    subset ss1 s -> subset ss2 s ->
+    card s < card ss1 + card ss2 ->
+    exists x, Sets.In x ss1 /\ Sets.In x ss2.
   Proof.
-    intros * Hset Hset1 Hset2 Hsub1 Hsub2 Hcard.
-    enough (Hdisj: ~~(exists x, In x ss1 /\ In x ss2)).
-    - rewrite <- disjoint_correct in Hdisj |- *.
+    intros * Hsub1 Hsub2 Hcard.
+    enough (Hdisj: ~~(exists x, Sets.In x ss1 /\ Sets.In x ss2)).
+    - rewrite <- disjoint_false_correct in Hdisj |- *.
       destruct (disjoint _ _); auto.
       now exfalso; apply Hdisj.
-    - intros Hdisj; apply (disjoint_subset_length s) in Hdisj; auto.
+    - intros Hdisj; apply (disjoint_subset_card s) in Hdisj; auto.
       lia.
   Qed.
 
-  Corollary majority_intersect : forall (s ss1 ss2 : list A),
-    NoDup s -> NoDup ss1 -> NoDup ss2 ->
-    incl ss1 s -> incl ss2 s ->
-    length s < length ss1 * 2 -> length s < length ss2 * 2 ->
-    exists x, In x ss1 /\ In x ss2.
+  Corollary majority_intersect : forall (s ss1 ss2 : set A),
+    subset ss1 s -> subset ss2 s ->
+    card s < card ss1 * 2 -> card s < card ss2 * 2 ->
+    exists x, Sets.In x ss1 /\ Sets.In x ss2.
   Proof.
     intros; eapply quorum_intersect with (s := s); eauto; lia.
   Qed.
