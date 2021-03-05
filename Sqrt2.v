90397939c283b1259be8d545a8af077f69796071
whonore <wolfhonore@gmail.com>
2021-03-05 12:46:30 -0500

    Clean up Sqrt2

--- Sqrt2.v
+++ Sqrt2.v
@@ -15,7 +15,7 @@ Unset Printing Implicit Defensive.
 
 Lemma Qred_int x : Qred (x # 1) = x # 1.
 Proof.
-  case: x; auto; case; auto.
+  by case: x => //; case.
 Qed.
 
 Lemma Qmult_den p q r : p # q == r <-> p # 1 == (Z.pos q # 1) * r.
@@ -24,20 +24,17 @@ Proof.
   by rewrite !Qmake_Qdiv -[_ == r](Qmult_inj_l _ _ den) ?Qmult_div_r -?Qmake_Qdiv.
 Qed.
 
-Theorem sqrt2_irrational p q : Z.gcd p (Z.pos q) = 1%Z -> ~(p # q)^2 == 2 # 1.
+Lemma even_sqr_even p : Z.even (p * p) -> Z.even p.
 Proof.
-  move=> Hred; rewrite Qmult_den -Qred_eq_iff ?Qred_int.
-  case; rewrite ?Pos2Z.inj_mul; set q' := Z.pos q; move=> Heq.
-  have Heven: Z.even p.
-  { suff: Z.even (p * p) by rewrite Z.even_mul; case: orP; auto; case.
-    by rewrite Heq Z.even_mul; case: orP; auto.
-  }
-  apply Z.even_spec in Heven; case: Heven => k ?; subst.
-  have Heq': (k * k * 2 = q' * q')%Z by lia.
-  have Heven': Z.even q'.
-  { suff: Z.even (q' * q') by rewrite Z.even_mul; case: orP; auto; case.
-    by rewrite -Heq' Z.even_mul; case: orP; auto.
-  }
-  apply Z.even_spec in Heven'; case: Heven' => j Hq; subst q'.
-  move: Hred; rewrite Hq Z.gcd_mul_mono_l; lia.
+  by rewrite Z.even_mul; case: orP => //; case.
+Qed.
+
+Theorem sqrt2_irrational p q : Z.gcd p (Z.pos q) = 1%Z -> ~((p # q)^2 == 2 # 1).
+Proof.
+  set q' := Z.pos q; move=> Hred; rewrite Qmult_den -Qred_eq_iff ?Qred_int.
+  case; rewrite ?Pos2Z.inj_mul; move=> Heq; move: Hred.
+  have /Z.even_spec[k Hk]: Z.even p by apply even_sqr_even; rewrite Heq Z.even_mul orb_true_r.
+  have Heq': (q' * q' = k * k * 2)%Z by lia.
+  have /Z.even_spec[? Hq]: Z.even q' by apply even_sqr_even; rewrite Heq' Z.even_mul orb_true_r.
+  rewrite Hk Hq Z.gcd_mul_mono_l; lia.
 Qed.
